fcd489476796b05017ef31a37e6cba6e583e12fd
yza11050618 <valdezyza4@gmail.com>
2025-03-11 15:52:32 +0800

    initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+//  burger menu
+
+const burger = document.querySelector(".burger__menu");
+const headerA = document.querySelector(".header__action");
+burger.addEventListener("click", () => {
+  burger.classList.toggle("open");
+  headerA.classList.toggle("open");
+});
